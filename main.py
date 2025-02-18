15c8220070fd7e23231852653735df8314c99930
Zephyros1938 <xtrau505true@gmail.com>
2025-02-18 15:48:02 -0600

    Update Implications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import tsapp, tsappMod
+import tsapp, tsappMod, pygame
 import random
 
-display = tsapp.GraphicsWindow(width=1280,height=720,background_color=(255,0,0))
+display = tsappMod.Surface(width=1280,height=720,background_color=(255,0,0))
 
 p = tsappMod.PolygonalObject(
     points=[
@@ -15,14 +15,29 @@ p = tsappMod.PolygonalObject(
     show_center=True,
     show_speed=True)
 
+p2 = tsappMod.PolygonalObject(
+    points=[
+        [-128,-128],
+        [128,-128],
+        [128,128],
+        [-128,128]
+    ],
+    color=(255,255,0),
+    linewidth=0,
+    show_center=True,
+    show_speed=True)
+
 tl = tsapp.TextLabel("Arial.ttf", 25, 0, 25, 1280, "EMPTY")
 fps_meter =tsapp.TextLabel("Arial.ttf", 25, 0, 25, 1280 ,"FPS_METER", (0,255,0))
 fps_meter.align = "right"
-display.framerate = 60
+display.framerate = 120
 
 display.add_object(p)
+display.add_object(p2)
 display.add_object(tl)
 display.add_object(fps_meter)
+
+gui_update_increment = 0
 
 while display.is_running:
     if(tsapp.is_key_down(tsapp.K_p)): exit()
@@ -30,7 +45,10 @@ while display.is_running:
     if(tsapp.is_key_down(tsapp.K_RIGHT) or tsapp.is_key_down(tsapp.K_d)): p.x_speed += 20
     if(tsapp.is_key_down(tsapp.K_UP) or tsapp.is_key_down(tsapp.K_w)): p.y_speed -= 20
     if(tsapp.is_key_down(tsapp.K_DOWN) or tsapp.is_key_down(tsapp.K_s)): p.y_speed += 20
-    tl.text = "POS:\n.    X:" + str(int(p.center_x)) + "\n.    Y:" + str(int(p.center_y)) + "\nSPEED:\n.    X:" + str(int(p.x_speed)) + "\n.    Y:" + str(int(p.y_speed))
-    fps_meter.text = "FPS: " + str(display._clock.get_fps())
-
+    if(gui_update_increment>=60):
+        tl.text = "POS:\n.    X:" + str(int(p.center_x)) + "\n.    Y:" + str(int(p.center_y)) + "\nSPEED:\n.    X:" + str(int(p.x_speed)) + "\n.    Y:" + str(int(p.y_speed))
+        fps_meter.text = "FPS: " + str(display._clock.get_fps())
+        gui_update_increment=0
+    p2.speed = (p.center_x - p2.center_x, p.center_y - p2.center_y)
+    gui_update_increment+=1
     display.finish_frame()
--- tsappMod.py
+++ tsappMod.py
@@ -2,7 +2,15 @@
 import tsapp;
 import pygame;
 
+class Surface(tsapp.GraphicsWindow):
+    def __init__(self, width=1018, height=573, background_color=tsapp.WHITE):
+        super().__init__(width,height,background_color)
+    
+    def finish_frame(self):
+        super().finish_frame()
+
 class PolygonalObject(tsapp.GraphicalObject):
+    world_coord_list = []
     def __init__(self, points = [[0,0],[1,0],[0,1]], center = [0,0], color = (255,255,255), linewidth = 0, show_center = False, show_speed = False):
         super().__init__()
         self.points = points
@@ -23,10 +31,10 @@ class PolygonalObject(tsapp.GraphicalObject):
     def _draw(self):
         surface = tsapp._get_window()._surface
         if(self.visible):
-            temp_coord_list = []
+            self.world_coord_list = []
             for i in self.points:
-                temp_coord_list.append((i[0] + (self.center_x), i[1] + (self.center_y)))
-            pygame.draw.polygon(surface, self.color, temp_coord_list, self.linewidth)
+                self.world_coord_list.append((i[0] + (self.center_x), i[1] + (self.center_y)))
+            pygame.draw.polygon(surface, self.color, self.world_coord_list, self.linewidth)
         if(self.show_center):
             pygame.draw.circle(surface, (0,255,0), (self.center_x, self.center_y), 2)
         if(self.show_speed):
@@ -36,3 +44,6 @@ class PolygonalObject(tsapp.GraphicalObject):
         x_speed, y_speed = self.speed
         self.center_x += (x_speed / 1000) * delta_time
         self.center_y += (y_speed / 1000) * delta_time
+    
+    def is_colliding_polygon(self, other_polygon):
+        pass
