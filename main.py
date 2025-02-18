695e3341c82d85f757ed4b438c794683edc60395
Zephyros1938 <xtrau505true@gmail.com>
2025-02-18 13:47:43 -0600

    Update to show cube positions

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ p = tsappMod.PolygonalObject(
     show_center=True,
     show_speed=True)
 
+tl = tsapp.TextLabel("Arial.ttf", 25, 0, 25, 1280, "EMPTY")
+
 display.add_object(p)
+display.add_object(tl)
 
 while display.is_running:
     if(tsapp.is_key_down(tsapp.K_p)): exit()
@@ -25,5 +28,6 @@ while display.is_running:
     if(tsapp.is_key_down(tsapp.K_RIGHT) or tsapp.is_key_down(tsapp.K_d)): p.x_speed += 20
     if(tsapp.is_key_down(tsapp.K_UP) or tsapp.is_key_down(tsapp.K_w)): p.y_speed -= 20
     if(tsapp.is_key_down(tsapp.K_DOWN) or tsapp.is_key_down(tsapp.K_s)): p.y_speed += 20
+    tl.text = "POS:\n.    X:" + str(int(p.center_x)) + "\n.    Y:" + str(int(p.center_y)) + "\nSPEED:\n.    X:" + str(int(p.x_speed)) + "\n.    Y:" + str(int(p.y_speed))
 
     display.finish_frame()
