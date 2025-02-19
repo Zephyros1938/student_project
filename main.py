d88a8355ed18c38477d05fea58687b32364195e3
Xtrau505 <xtrau505true@gmail.com>
2025-02-18 21:35:22 -0600

    Add keycodes, documentation, and extra testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tsapp, tsappMod, pygame
 import random
 
-display = tsappMod.Surface(width=1280,height=720,background_color=(255,0,0))
+display = tsappMod.Surface(width=3840 ,height=1600,background_color=(0,0,0))
 
 p = tsappMod.PolygonalObject(
     points=[
@@ -19,36 +19,52 @@ p2 = tsappMod.PolygonalObject(
     points=[
         [-128,-128],
         [128,-128],
-        [128,128],
-        [-128,128]
+        [0,128],
     ],
-    color=(255,255,0),
+    color=(128,0,128),
     linewidth=0,
     show_center=True,
     show_speed=True)
 
-tl = tsapp.TextLabel("Arial.ttf", 25, 0, 25, 1280, "EMPTY")
-fps_meter =tsapp.TextLabel("Arial.ttf", 25, 0, 25, 1280 ,"FPS_METER", (0,255,0))
-fps_meter.align = "right"
-display.framerate = 120
+text_bg = tsappMod.PolygonalObject(
+    points=[
+        [0,0],
+        [display.width,0],
+        [display.width,30],
+        [0,30]
+    ],
+    color=(255,255,255),
+    linewidth=0,
+    show_center=True,
+    show_speed=True)
 
-display.add_object(p)
-display.add_object(p2)
+tl = tsapp.TextLabel("CourierNew.ttf", 25, 0, 25, display.width, "EMPTY")
+fps_meter =tsapp.TextLabel("CourierNew.ttf", 25, 0, 25, display.width ,"FPS_METER", (0,0,0))
+fps_meter.align = "right"
+display.framerate = 30
+
+display.add_object(text_bg)
 display.add_object(tl)
 display.add_object(fps_meter)
+display.add_object(p)
+display.add_object(p2)
 
 gui_update_increment = 0
 
 while display.is_running:
-    if(tsapp.is_key_down(tsapp.K_p)): exit()
+    if(tsapp.is_key_down(tsappMod.K_ESCAPE)): exit()
     if(tsapp.is_key_down(tsapp.K_LEFT) or tsapp.is_key_down(tsapp.K_a)): p.x_speed -= 20
     if(tsapp.is_key_down(tsapp.K_RIGHT) or tsapp.is_key_down(tsapp.K_d)): p.x_speed += 20
     if(tsapp.is_key_down(tsapp.K_UP) or tsapp.is_key_down(tsapp.K_w)): p.y_speed -= 20
     if(tsapp.is_key_down(tsapp.K_DOWN) or tsapp.is_key_down(tsapp.K_s)): p.y_speed += 20
-    if(gui_update_increment>=60):
-        tl.text = "POS:\n.    X:" + str(int(p.center_x)) + "\n.    Y:" + str(int(p.center_y)) + "\nSPEED:\n.    X:" + str(int(p.x_speed)) + "\n.    Y:" + str(int(p.y_speed))
+    if(p.center_x>=display.width+20): p.center_x=0
+    if(p.center_x<=-20): p.center_x = display.width
+    if(p.center_y<=-20): p.center_y = display.height
+    if(p.center_y>=display.height+20): p.center_y = 0
+    if(gui_update_increment>=display.framerate):
+        tl.text = "X :" + str(int(p.center_x)) + " Y :" + str(int(p.center_y))
         fps_meter.text = "FPS: " + str(display._clock.get_fps())
         gui_update_increment=0
-    p2.speed = (p.center_x - p2.center_x, p.center_y - p2.center_y)
+    p2.speed = (p.center_x*1.25 - p2.center_x, p.center_y*1.25 - p2.center_y)
     gui_update_increment+=1
     display.finish_frame()
--- tsappMod.py
+++ tsappMod.py
@@ -1,17 +1,123 @@
+"""
+    MADE BY ZEPHYROS1938 (zephyros@zephyros1938.org)
+    FREE TO USE, WITH CREDIT.
+"""
+import tsapp
+import pygame
 
-import tsapp;
-import pygame;
+# Keys
+
+if(pygame.get_init()==False): pygame.init()
+
+K_TAB = pygame.K_TAB
+K_ESCAPE = pygame.K_ESCAPE
+K_BACKSPACE = pygame.K_BACKSPACE
+K_DELETE = pygame.K_DELETE
+K_INSERT = pygame.K_INSERT
+K_HOME = pygame.K_HOME
+K_END = pygame.K_END
+K_PAGEUP = pygame.K_PAGEUP
+K_PAGEDOWN = pygame.K_PAGEDOWN
+K_CAPSLOCK = pygame.K_CAPSLOCK
+K_PRINT = pygame.K_PRINT
+K_PAUSE = pygame.K_PAUSE
+K_SCROLLLOCK = pygame.K_SCROLLLOCK
+
+K_F1 = pygame.K_F1
+K_F2 = pygame.K_F2
+K_F3 = pygame.K_F3
+K_F4 = pygame.K_F4
+K_F5 = pygame.K_F5
+K_F6 = pygame.K_F6
+K_F7 = pygame.K_F7
+K_F8 = pygame.K_F8
+K_F9 = pygame.K_F9
+K_F10 = pygame.K_F10
+K_F11 = pygame.K_F11
+K_F12 = pygame.K_F12
+
+K_LSHIFT = pygame.K_LSHIFT
+K_RSHIFT = pygame.K_RSHIFT
+K_LCTRL = pygame.K_LCTRL
+K_RCTRL = pygame.K_RCTRL
+K_LALT = pygame.K_LALT
+K_RALT = pygame.K_RALT
+K_LMETA = pygame.K_LMETA
+K_RMETA = pygame.K_RMETA
+
+K_KP0 = pygame.K_KP0
+K_KP1 = pygame.K_KP1
+K_KP2 = pygame.K_KP2
+K_KP3 = pygame.K_KP3
+K_KP4 = pygame.K_KP4
+K_KP5 = pygame.K_KP5
+K_KP6 = pygame.K_KP6
+K_KP7 = pygame.K_KP7
+K_KP8 = pygame.K_KP8
+K_KP9 = pygame.K_KP9
+K_KP_PLUS = pygame.K_KP_PLUS
+K_KP_MINUS = pygame.K_KP_MINUS
+K_KP_MULTIPLY = pygame.K_KP_MULTIPLY
+K_KP_DIVIDE = pygame.K_KP_DIVIDE
+K_KP_ENTER = pygame.K_KP_ENTER
+K_KP_PERIOD = pygame.K_KP_PERIOD
+
+K_MINUS = pygame.K_MINUS
+K_EQUALS = pygame.K_EQUALS
+K_LEFTBRACKET = pygame.K_LEFTBRACKET
+K_RIGHTBRACKET = pygame.K_RIGHTBRACKET
+K_BACKSLASH = pygame.K_BACKSLASH
+K_SEMICOLON = pygame.K_SEMICOLON
+K_QUOTE = pygame.K_QUOTE
+K_COMMA = pygame.K_COMMA
+K_PERIOD = pygame.K_PERIOD
+K_SLASH = pygame.K_SLASH
+K_BACKQUOTE = pygame.K_BACKQUOTE
+
+# Tsapp Overrides
 
 class Surface(tsapp.GraphicsWindow):
+    """
+    A class to represent a graphical window surface; Modifies Tsapp's GraphicsWindow.
+
+    Attributes:
+    width : int
+        The width of the window.
+    height : int
+        The height of the window.
+    background_color : tuple
+        The background color of the window.
+    """
     def __init__(self, width=1018, height=573, background_color=tsapp.WHITE):
-        super().__init__(width,height,background_color)
+        super().__init__(width, height, background_color) 
     
     def finish_frame(self):
         super().finish_frame()
 
+# Missing Tsapp Objects
+
 class PolygonalObject(tsapp.GraphicalObject):
+    """
+    A class to represent a polygonal graphical object.
+
+    Attributes:
+    points : list
+        List of points defining the polygon.
+    center : list
+        The center coordinates of the polygon.
+    color : tuple
+        The color of the polygon.
+        (R,G,B)
+    linewidth : int
+        The width of the polygon lines.
+    show_center : bool
+        Flag to show the center of the polygon.
+    show_speed : bool
+        Flag to show the speed vector of the polygon.
+    """
     world_coord_list = []
-    def __init__(self, points = [[0,0],[1,0],[0,1]], center = [0,0], color = (255,255,255), linewidth = 0, show_center = False, show_speed = False):
+    
+    def __init__(self, points=[[0,0],[1,0],[0,1]], center=[0,0], color=(255,255,255), linewidth=0, show_center=False, show_speed=False):
         super().__init__()
         self.points = points
         temp_center_x = 0
